0f894410b58713fe2fac0f17b45e4681d2495262
jerrymao15 <jerryy.mao@gmail.com>
2016-10-06 21:26:31 -0700

    more PUT tests

--- test/articleRoute-Test.js
+++ test/articleRoute-Test.js
@@ -29,13 +29,9 @@ describe('Article GET requests', function() {
     .expect('Content-Type', /json/)
     .expect(200)
     .expect(res => {
-      try {
-        const result = JSON.parse(res.text)
-        expect(result.length).to.eql(1);
-        expect(result[0].author.name).to.eql('Amy Kovacek');
-      } catch (e) {
-        throw new Error(e);
-      }
+      const result = JSON.parse(res.text)
+      expect(result.length).to.eql(1);
+      expect(result[0].author.name).to.eql('Amy Kovacek');
     })
     .end(done)
   })
@@ -64,20 +60,70 @@ describe('Article PUT requests', function() {
     .then(() => done());
   });
 
-  it('should edit the correct file', function() {
+  it('should edit the correct file', function(done) {
+    const edit = {
+      title: 'This is a changed title',
+      likes_count: 100,
+      summary: 'Cats are good for your health.',
+    };
 
+    request.put('/v1/articles/15')
+    .send(edit)
+    .expect(200)
+    .expect(res => {
+      const { title, likes_count, summary } = JSON.parse(res.text)[0];
+      expect(title).to.eql(edit.title);
+      expect(likes_count).to.eql(edit.likes_count)
+      expect(summary).to.eql(edit.summary);
+    })
+    .end(done);
   });
 
-  it('should only edit the changed queries', function() {
+  it('should only edit the changed properties', function(done) {
+    const edit = {
+      title: 'This is a changed title',
+      likes_count: 100,
+      summary: 'Cats are good for your health.',
+    };
 
+    request.put('/v1/articles/15')
+    .send(edit)
+    .expect(200)
+    .expect(res => {
+      const { author: { name }, media_url } = JSON.parse(res.text)[0];
+      expect(name).to.eql(articleFixture.author_name);
+      expect(media_url).to.eql(articleFixture.media_url)
+    })
+    .end(done);
   });
 
-  it('should disregard unrelated queries', function() {
+  it('should disregard unrelated properties', function(done) {
+    const edit = {
+      title: 'This is a changed title',
+      likes_count: 100,
+      summary: 'Cats are good for your health.',
+      uhoh: 'This should not be here.',
+    };
 
+    request.put('/v1/articles/15')
+    .send(edit)
+    .expect(200)
+    .expect(res => {
+      const { title, uhoh } = JSON.parse(res.text)[0];
+      expect(title).to.eql(edit.title);
+      expect(uhoh).to.be.undefined;
+    })
+    .end(done);
   })
 
-  it('should handle trying to edit a nonexsiting article', function() {
-
+  it('should handle trying to edit a nonexsiting article', function(done) {
+    const edit = {
+      summary: 'What am I updating...?',
+    };
+    request.put('/v1/articles/105')
+    .send(edit)
+    .expect(400)
+    .end(done);
   });
 
 
