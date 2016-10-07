3143e825b7c0c4c8e006c9cd0d634f81011fbcb6
jerrymao15 <jerryy.mao@gmail.com>
2016-10-07 01:51:48 -0700

    auth finished

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,7 +1,9 @@
-
 const authController = {
   validateToken(req, res, next) {
+    if (process.env.SECRET_TOKEN === undefined) require('dotenv').config();
 
+    const validated = `bearer ${process.env.SECRET_TOKEN}` === req.get('Authorization');
+    return validated ? next() : res.status(401).end('401 Unauthorized');
   },
 };
 
