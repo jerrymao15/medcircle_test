d336af4183736f1a71279aa1fe78ccda6d5228f1
jerrymao15 <jerryy.mao@gmail.com>
2016-10-06 17:49:25 -0700

    test boilerplate

---
+++ test/articleRoute-Test.js
@@ -0,0 +1,15 @@
+const { expect } = require('chai');
+const request = require('supertest');
+
+describe('Article GET requests', function() {
+
+});
+
+
+describe('Article PUT requests', function() {
+
+});
+
+describe('Article DELETE requests', function() {
+
+});
