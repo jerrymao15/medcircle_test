bd35f92ddbdb0c987ab4a9cf6e3a8f58a1c6026b
jerrymao15 <jerryy.mao@gmail.com>
2016-10-06 17:49:04 -0700

    environment changes, initial server file

--- server/controllers/createController.js
+++ server/controllers/createController.js
@@ -7,7 +7,7 @@ const createController = {
   createDB(req, res, next) {
     axios.get(medcicleExampleURI)
     .then((response) => {
-      const articlesArray = response.data.map((articles) => {
+      const articlesArray = response.data.map(articles => {
         const { id, title, summary, media_url, published_at, likes_count, author: { name: author_name, icon_url: author_icon_url } } = articles;
         return { id, title, summary, media_url, published_at, likes_count, author_name, author_icon_url };
       });
@@ -15,6 +15,7 @@ const createController = {
       return sequelize.sync();
     })
     .then(() => Article.bulkCreate(req.articlesArray))
+    .then(() => console.log('Database created!'))
     .then(() => next())
     .catch(error => console.error(error));
   },
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const authController = require('./controllers/authController');
+const articleRoutes = require('./v1/articleRoutes');
 const createController = require('./controllers/createController');
 
 const app = express();
@@ -10,12 +10,9 @@ const PORT = 3000;
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, './../')));
 
+app.use('/v1', articleRoutes);
+
+// used to populate the database on start.
 app.get('/initialize', createController.createDB, (req, res) => res.end());
-
-
-
-
-
-
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}.`));
